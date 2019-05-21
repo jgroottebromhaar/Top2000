50188b420d1acc11713d1fb7b0ba8808f7bdd54f
jgroottebromhaar <jiri_gb_94@hotmail.com>
2019-05-21 11:34:25 +0200

    Added comment

--- Top2000/Top2000/Controllers/HomeController.cs
+++ Top2000/Top2000/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace Top2000.Controllers
 {
+    /// <summary>
+    /// The HomeController
+    /// </summary>
     public class HomeController : Controller
     {
         public ActionResult Index()
